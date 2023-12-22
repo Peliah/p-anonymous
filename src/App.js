88c3ea8fed9286a66519bdbe1c9d5712de5c0b28
Peliah <pelepoupa@gmail.com>
2023-12-22 22:53:30 +0100

    "commit lazy"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="w-full h-full justify-center items-center text-blue-700 font-bold text-xl font-mono">
+      <p>Nothing Here Yet</p>
     </div>
   );
 }
