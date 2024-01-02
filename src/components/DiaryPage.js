7ceaeb72fc8561973c0930ccb69afecd4557b40d
Peliah <pelepoupa@gmail.com>
2024-01-02 01:48:10 +0100

    first test

--- src/components/DiaryPage.js
+++ src/components/DiaryPage.js
@@ -18,13 +18,13 @@ const DiaryPage = React.forwardRef((props, ref) => {
     }
 
   return (
-    <div className='flex justify-center items-center w-full' ref={ref}>
-        <form onSubmit={onSubmit} className='flex flex-col justify-center items-center'>
-            <div className="diary-form">
-                <input value={title} onChange={(event) => setTitle(event.target.value)} placeholder="Add an Item" className="border rounded p-4 h-14 mr-9 w-7/12 shadow-sm"/>
-                <input value={date} onChange={(event) => setDate(event.target.value)} type="date" className="border rounded p-1 h-14 w-4/12 shadow-sm"/>
+    <div className='flex justify-center items-center w-full h-full p-4 bg-white' ref={ref}>
+        <form onSubmit={onSubmit} className='flex flex-col justify-center items-center h-full'>
+            <div className="w-full">
+                <input value={title} onChange={(event) => setTitle(event.target.value)} placeholder="Add an Item" className="border rounded p-4 h-14 mr-5 w-7/12 shadow-sm"/>
+                <input value={date} onChange={(event) => setDate(event.target.value)} type="date" className="border rounded p-1 h-14 w-4/12 shadow-sm ml-7"/>
             </div>
-            <textarea value={text} onChange={(event) => setText(event.target.value)} rows="2" className="p-6 mt-4 border rounded w-full shadow">
+            <textarea value={text} onChange={(event) => setText(event.target.value)} rows="2" className="p-6 mt-4 border rounded w-full shadow h-full">
             </textarea>
             <button type="submit" className="items-center p-2 text-lg border w-full rounded-lg mt-7 cursor-pointer bg-black text-white capitalize ">Save</button>
         </form>
--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from "react";
 import DiaryPage from "../components/DiaryPage";
 import HTMLFlipBook from 'react-pageflip';
+import Navbar from "../components/Navbar";
 
 
 const Diary = (props) => {
@@ -22,25 +23,30 @@ const Diary = (props) => {
   };
 
   return (
-    <div className="">
-      <nav className='flex w-full items-center justify-between fixed bg-black px-4 lg:h-20 h-20'>
+    <div >
+      <nav className='flex w-full items-center justify-between fixed px-4 lg:h-20 h-20 bg-black'>
         <div>
             <img src={require('../assets/images/fontbolt (2).png')} alt='logo' className='h-8 hidden lg:block'/>
         </div>
         <div>
-          <button className='border rounded-lg px-8 py-3 text-center font-extrabold text-white font-death-note'>
-            GUIDELINES
+          <button className='border rounded-lg px-8 py-3 text-center font-bold text-white font-roboto-mono'>
+            Open Diary
           </button>
         </div>
-    </nav>
-    <div className=" flex justify-center items-center min-h-screen font-roboto-mono">
-      <HTMLFlipBook width={500} height={500} ref={flipBookRef} usePortrait size="fixed" showCover={true}>
-        {generateDiaryPages(100)}
-      </HTMLFlipBook> 
-      <button onClick={() => goToPage(10)}>Go to Page 10</button>
-    </div>
-      {/* {/* <DiaryPage/> */}
-      
+      </nav>
+      {/* <Navbar/> */}
+      <div className=" flex justify-center items-center min-h-screen font-roboto-mono bg-black pt-20">
+        <HTMLFlipBook width={window.innerWidth/3} height={window.innerHeight-200} useMouseEvents={true} ref={flipBookRef} usePortrait size="fixed" showCover={true} className="w-full bg-black rounded-lg">
+          <img src={require('../assets/images/deathnote_cover_for_download_by_deathnote_club_d1xmeas.jpg')}/>
+          <img src={require('../assets/images/deathnote_rules___page_1_by_deathnote_club_d1xmein.jpg')}/>
+          <img src={require('../assets/images/deathnote_rules___page_2_by_deathnote_club_d1xmply.jpg')}/>
+          <img src={require('../assets/images/deathnote_rules___page_3_by_deathnote_club_d1xmpp8.jpg')}/>
+          <img src={require('../assets/images/deathnote_rules___page_4_by_deathnote_club_d1xmps7.jpg')}/>
+          <img src={require('../assets/images/deathnote_rules___page_5_by_deathnote_club_d1xmpug.jpg')}/>
+          {generateDiaryPages(100)}
+        </HTMLFlipBook> 
+        {/* <button onClick={() => goToPage(10)}>Go to Page 10</button> */}
+      </div>
     </div>
   )
 }
