0ad3482c6856742a10fa9569470a97bb050df83f
Peliah <pelepoupa@gmail.com>
2024-01-28 12:01:57 +0100

    big changes coming soon.

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -37,17 +37,18 @@ const Diary = (props) => {
       {/* <Navbar/> */}
       <div className=" flex justify-center items-center min-h-screen font-roboto-mono bg-black lg:bg-white pt-10 lg:pt-20 rounded-lg flex-col">
       <div className="flex items-center justify-evenly w-full">
-        <img src={require('../assets/images/fontbolt (4).png')} alt="left" className="w-20" onClick={ () => flipBookRef.current.pageFlip().flipPrev('top')}/>
-        <img src={require('../assets/images/fontbolt (3).png')} alt="right" className="w-20" onClick={() => flipBookRef.current.pageFlip().flipNext('top')}/>
-
+        <img src={require('../assets/images/fontbolt (4).png')} alt="left" className="w-14 lg:w-20 hidden lg:block " onClick={ () => flipBookRef.current.pageFlip().flipPrev('top')}/>
+        <img src={require('../assets/images/fontbolt (3).png')} alt="right" className="w-14 lg:w-20 hidden lg:block " onClick={() => flipBookRef.current.pageFlip().flipNext('top')}/>
+        <img src={require('../assets/images/fontbolt (9).png')} alt="left" className="w-14 lg:w-20 block lg:hidden " onClick={ () => flipBookRef.current.pageFlip().flipPrev('top')}/>
+        <img src={require('../assets/images/fontbolt (8).png')} alt="right" className="w-14 lg:w-20 block lg:hidden " onClick={() => flipBookRef.current.pageFlip().flipNext('top')}/>
       </div>
         <HTMLFlipBook width={window.innerWidth > 600 ? window.innerWidth/3 : window.innerWidth} height={window.innerHeight-200} useMouseEvents={false} ref={flipBookRef} size="fixed" showCover={true} mobileScrollSupport={true} className="w-full bg-white rounded-lg">
-          <img src={require('../assets/images/deathnote_cover_for_download_by_deathnote_club_d1xmeas.jpg')} className="rounded-lg h-fit"/>
-          <img src={require('../assets/images/deathnote_rules___page_1_by_deathnote_club_d1xmein.jpg')} className="rounded-lg h-fit"/>
-          <img src={require('../assets/images/deathnote_rules___page_2_by_deathnote_club_d1xmply.jpg')} className="rounded-lg h-fit"/>
-          <img src={require('../assets/images/deathnote_rules___page_3_by_deathnote_club_d1xmpp8.jpg')} className="rounded-lg h-fit"/>
-          <img src={require('../assets/images/deathnote_rules___page_4_by_deathnote_club_d1xmps7.jpg')} className="rounded-lg h-fit"/>
-          <img src={require('../assets/images/deathnote_rules___page_5_by_deathnote_club_d1xmpug.jpg')} className="rounded-lg h-fit"/>
+          <img src={require('../assets/images/deathnote_cover_for_download_by_deathnote_club_d1xmeas.jpg')} className="rounded-lg h-fit" alt="deathnote guidelines"/>
+          <img src={require('../assets/images/deathnote_rules___page_1_by_deathnote_club_d1xmein.jpg')} className="rounded-lg h-fit" alt="deathnote guidelines"/>
+          <img src={require('../assets/images/deathnote_rules___page_2_by_deathnote_club_d1xmply.jpg')} className="rounded-lg h-fit" alt="deathnote guidelines"/>
+          <img src={require('../assets/images/deathnote_rules___page_3_by_deathnote_club_d1xmpp8.jpg')} className="rounded-lg h-fit" alt="deathnote guidelines"/>
+          <img src={require('../assets/images/deathnote_rules___page_4_by_deathnote_club_d1xmps7.jpg')} className="rounded-lg h-fit" alt="deathnote guidelines"/>
+          <img src={require('../assets/images/deathnote_rules___page_5_by_deathnote_club_d1xmpug.jpg')} className="rounded-lg h-fit" alt="deathnote guidelines"/>
           {generateDiaryPages(100)}
         </HTMLFlipBook> 
         {/* <button onClick={() => goToPage(10)}>Go to Page 10</button> */}
